eb39b7f2ed87446b39553ecf50f7b5a88c76c41c
pawlika <pawlik.addam@gmail.com>
2022-04-28 08:42:54 +0200

    added in words.service BehaviorSubject and Subject

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -1,32 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {WordsService} from "../../services/words.service";
 import {WordType} from "../../data/models";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-question',
   templateUrl: './question.component.html',
   styleUrls: ['./question.component.css']
 })
-export class QuestionComponent implements OnInit {
+export class QuestionComponent implements OnInit,OnDestroy {
 
-  word:WordType=null;
-  constructor(private wordService: WordsService) { }
+  word: WordType = null;
+  private words = [];
+  private subscription: Subscription;
+
+  constructor(private wordService: WordsService) {
+  }
 
   ngOnInit(): void {
-    this.fetchWord();
+    this.subscription = this.wordService.getWords().subscribe((words) => {
+      this.words = words;
+      this.fetchWord();
+    })
   }
-private fetchWord():void{
-    this.word=this.wordService.getWords().shift();
-}
-addToNoun(wordd:WordType){
+
+  private fetchWord(): void {
+    this.word = this.words.shift();
+  }
+
+  addToNoun(wordd: WordType) {
     this.wordService.addNoun(wordd);
     this.fetchWord();
-}
-addToVerb(word:WordType){
+  }
+
+  addToVerb(word: WordType) {
     this.wordService.addVerb(word);
     this.fetchWord();
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe()
+  }
+
 }
-check(){
-    this.wordService.check();
-}
-}
+
+
--- src/app/services/words.service.ts
+++ src/app/services/words.service.ts
@@ -1,42 +1,54 @@
 import {Injectable} from '@angular/core';
 import {Type, WordType} from "../data/models";
-import {WORDS} from "../data/data-bsae";
+import {WORDS} from "../data/data-base";
+import {BehaviorSubject, Observable, Subject} from "rxjs";
+import {map} from "rxjs/operators";
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class WordsService {
 
-  private words: WordType[] = [];
-  private nouns: WordType[] = [];
-  private verbs: WordType[] = [];
+  private words = new BehaviorSubject<WordType[]>([]);
+  private nouns = new Subject<WordType>();
+  private verbs = new Subject<WordType>();
 
   constructor() {
-    this.words = WORDS;
+    setTimeout(() => {
+      this.words.next(WORDS);
+    }, 1000)
+
   }
 
-  getWords(): WordType[] {
+  getWords(): BehaviorSubject<WordType[]> {
     return this.words
   }
 
-  getNoun(): WordType[] {
-    return this.nouns
+  getNoun(): Observable<WordType> {
+    return this.nouns.asObservable().pipe(
+      map(word => {
+        word.correct = word.type === Type.NOUN
+        return word;
+      })
+    )
   }
 
-  getVerb(): WordType[] {
-    return this.verbs;
+  getVerb(): Observable<WordType> {
+    return this.verbs.asObservable().pipe(
+      map(word => {
+        word.correct = word.type === Type.VERB
+        return word;
+      })
+    );
   }
 
   addNoun(value: WordType): void {
-    this.nouns.push(value);
+    this.nouns.next(value);
   }
 
   addVerb(value: WordType): void {
-    this.verbs.push(value);
+    this.verbs.next(value);
   }
 
-  check() {
-    this.nouns.map(word => (word.correct = word.type === Type.NOUN));
-    this.verbs.map(word => (word.correct = word.type === Type.VERB));
-  }
 }
